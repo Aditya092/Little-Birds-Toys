e47e43aaf5ba1a17f44986d24569ab3ecda93e90
523dnaruka <92735686+523dnaruka@users.noreply.github.com>
2022-02-09 10:42:35 -0500

    Add files via upload

--- Toys.js
+++ Toys.js
@@ -1,36 +1,36 @@
 function clearName() {
-      if (document.getElementById('nameField').value == 'First and Last Name') {
+      if (document.getElementById('nameField').value == 'John Smith') {
         document.getElementById('nameField').value = '';
       }
     }
 
     function resetName() {
       if (document.getElementById('nameField').value == '') {
-        document.getElementById('nameField').value = 'First and Last Name';
+        document.getElementById('nameField').value = 'John Smith';
       }
     }
 
     function clearPhone() {
-      if (document.getElementById('phoneField').value == 'example: 212-123-1234') {
+      if (document.getElementById('phoneField').value == 'XXX-XXX-XXXX') {
         document.getElementById('phoneField').value = '';
       }
     }
 
     function resetPhone() {
       if (document.getElementById('phoneField').value == '') {
-        document.getElementById('phoneField').value = 'example: 212-123-1234';
+        document.getElementById('phoneField').value = 'XXX-XXX-XXXX';
       }
     }
 
     function clearEmail() {
-      if (document.getElementById('emailField').value == 'example: johnsmith@email.com') {
+      if (document.getElementById('emailField').value == 'johnsmith@email.com') {
         document.getElementById('emailField').value = '';
       }
     }
 
     function resetEmail() {
       if (document.getElementById('emailField').value == '') {
-        document.getElementById('emailField').value = 'example: johnsmith@email.com';
+        document.getElementById('emailField').value = 'johnsmith@email.com';
       }
     }
 
